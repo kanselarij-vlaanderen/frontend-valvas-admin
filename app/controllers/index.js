8ae21cbbc659adf6be4e5a6acfa25d54a05f146e
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-09 17:45:49 +0200

    Fix linting errors

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,34 +1,33 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
-
 export default class IndexController extends Controller {
   constructor() {
-    super(...arguments)
+    super(...arguments);
     this.zittingId = '';
-    this.exportType = 'all'
+    this.exportType = 'all';
     this.documentNotification = false;
-    this.documentPublicationDateTime = ''
-    this.sessionDate = ''
-    this.previewNotification = false
+    this.documentPublicationDateTime = '';
+    this.sessionDate = '';
+    this.previewNotification = false;
     this.scopes = {
       'news-items': ['news-items'],
       'announcements': ['announcements'],
       'news-items-and-announcements' : [ 'news-items', 'announcements' ],
       'all' : [ 'news-items', 'announcements', 'documents' ]
-    }
+    };
   }
 
   @action
   exportZitting() {
     const body= {
       scope: this.scopes[this.exportType]
-    }
+    };
     if(this.documentNotification) {
       body.documentNotification = {
         sessionDate: this.sessionDate,
         documentPublicationDateTime: this.documentPublicationDateTime
-      }
+      };
     }
     fetch(`/export/${this.zittingId}`, {
       method: 'POST',
@@ -36,27 +35,27 @@ export default class IndexController extends Controller {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(body)
-    })
+    });
   }
 
   updateZittingId(value) {
-    this.set('zittingId', value)
+    this.set('zittingId', value);
   }
 
   updateDocumentNotification(value) {
-    this.set('documentNotification', value)
+    this.set('documentNotification', value);
   }
 
   updateSessionDate(value) {
-    this.set('sessionDate', value)
+    this.set('sessionDate', value);
   }
 
   updateDocumentPublicationDateTime(value) {
-    this.set('documentPublicationDateTime', value)
+    this.set('documentPublicationDateTime', value);
   }
-  
+
   updatePreviewNotification(value) {
-    this.set('previewNotification', value)
+    this.set('previewNotification', value);
   }
 
 }
